347c2a2d821759840484b9e0680ecf90c267fdb4
zaqttack <zaquariah@gmail.com>
2020-03-29 10:25:52 -0500

    first commit

---
+++ database.js
@@ -0,0 +1,3 @@
+Extends User;
+let user1 = new User(001,77082,ryan@gmail.com,3,10,50,100);
+user1.
---
+++ index.js
@@ -0,0 +1,9 @@
+function addClick() {
+    clicks[0] += 1;
+    document.getElementById("clickNumber").innerHTML = clicks[0];
+}
+
+function addClick2() {
+    clicks[1] += 1;
+    document.getElementById("clickNumber2").innerHTML = clicks[1];
+}
